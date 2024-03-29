b292ef64a9bfb722a29c53426ef958cb59abc7ff
Eric <real.dassy@gmail.com>
2024-03-29 22:48:27 +0900

    [BOJ_14681] 사분면 고르기

---
+++ Algorithm/BaekJoon/[BOJ_14681] 사분면 고르기/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 송지혁 on 3/29/24.
+//
+
+import Foundation
+
+let x = Int(readLine()!)!
+let y = Int(readLine()!)!
+
+print(x > 0 ? y > 0 ? "1" : "4" : y < 0 ? "3" : "2")
+
