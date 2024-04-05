1509eaf6031a9700bfdc6c3f7065b2051db0a485
Eric <real.dassy@gmail.com>
2024-04-05 20:35:05 +0900

    [BOJ_10811] 바구니 뒤집기
    원본 배열을 바꾸는 reverse() 함수를 사용했어여
    초기 배열 선언도 직관적인 방법을 선택했어요

--- Algorithm/BaekJoon/[BOJ_10811] 바구니 뒤집기/main.swift
+++ Algorithm/BaekJoon/[BOJ_10811] 바구니 뒤집기/main.swift
@@ -9,14 +9,11 @@ let NM = readLine()!.split(separator: " ").map { Int($0)! }
 let n = NM[0]
 let m = NM[1]
 
-var buckets: [Int] = []
+var buckets: Array<Int> = Array(1...n)
 
-(1...n).forEach { buckets.append($0) }
-
-for i in 0..<m {
+for _ in 0..<m {
     let swap_index = readLine()!.split(separator: " ").map { Int($0)! }
-    let slicedArr = Array(buckets[swap_index[0]-1...swap_index[1]-1]).reversed()
-    buckets.replaceSubrange(swap_index[0]-1...swap_index[1]-1, with: slicedArr)
+    buckets[swap_index[0]-1...swap_index[1]-1].reverse()
 }
 
 
