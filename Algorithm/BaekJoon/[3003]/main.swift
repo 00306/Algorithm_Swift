5fd68048ffcdfa14bb3a90fd02243cbf69d260cd
Eric <real.dassy@gmail.com>
2024-04-08 16:59:03 +0900

    [BOJ_3003] 킹, 퀸, 룩, 비숍, 나이트, 폰
    joined() 함수를 사용해보았어여

--- Algorithm/BaekJoon/[3003]/main.swift
+++ Algorithm/BaekJoon/[3003]/main.swift
@@ -6,11 +6,11 @@
 //
 
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-let pieces = [1, 1, 2, 2, 2, 8]
-var answer = ""
+var pieces = [1, 1, 2, 2, 2, 8]
+var answer: [String] = Array(repeating: "", count: 6)
 
 for i in 0..<pieces.count {
-    answer += "\(String(pieces[i]-input[i])) "
+    answer[i] = String(pieces[i]-input[i])
 }
 
-print(answer)
+print(answer.joined(separator: " "))
