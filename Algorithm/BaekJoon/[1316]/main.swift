ed775f82859825c63fe3ebfd1138c27ba7819165
Eric <real.dassy@gmail.com>
2024-04-09 00:46:14 +0900

    [BOJ_1316] 그룹 단어 체커

--- Algorithm/BaekJoon/[1316]/main.swift
+++ Algorithm/BaekJoon/[1316]/main.swift
@@ -5,21 +5,21 @@
 //  Created by 송지혁 on 4/9/24.
 //
 
-let n = Int(readLine()!)!
-var answer = 0
+var n = Int(readLine()!)!
 
 for _ in 0..<n {
     let word = readLine()!
-    let set = Set(word)
-    for char in set {
-        if !(word.filter({ $0 == char }).count == word[word.firstIndex(of: char)!...word.lastIndex(of: char)!].count) {
-            answer -= 1
+    var arr: [Character] = []
+    
+    for char in word {
+        if !arr.contains(char) {
+            arr.append(char)
+        } else if arr.last! != char {
+            n -= 1
             break
         }
     }
-    
-    answer += 1
-    
 }
 
-print(answer)
+
+print(n)
