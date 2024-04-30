6d07d6acb4db8f3a8f1e64c339522e2d833375cd
Eric <real.dassy@gmail.com>
2024-04-30 13:46:57 +0900

    [11478]
    String.index의 시간복잡도가 O(n)이라는 충격적인 사실..

---
+++ Algorithm/BaekJoon/[11478]/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 송지혁 on 4/30/24.
+//
+
+let s = readLine()!.map { String($0) }
+var str_set: Set<String> = []
+
+
+for i in 0..<s.count {
+    var str = ""
+    for j in i..<s.count {
+        str += s[j]
+        str_set.insert(str)
+    }
+    
+    
+}
+
+print(str_set.count)
+
+
+// brute force로 하면 시간초과
