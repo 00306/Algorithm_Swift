113dcd20243daaa761dc7470fb63a65d1daff6fb
Eric <real.dassy@gmail.com>
2024-05-02 13:50:59 +0900

    [11478]

--- Algorithm/BaekJoon/[11478]/main.swift
+++ Algorithm/BaekJoon/[11478]/main.swift
@@ -5,18 +5,17 @@
 //  Created by 송지혁 on 4/30/24.
 //
 
-let s = readLine()!.map { String($0) }
-var str_set: Set<String> = []
+let s = readLine()!
+var str_set: Set<Substring> = []
 
-
-for i in 0..<s.count {
-    var str = ""
-    for j in i..<s.count {
-        str += s[j]
-        str_set.insert(str)
+// 문자열 index
+for i in s.indices {
+    
+    // i 번째 문자 이후 문자열의 index
+    for j in s.indices[i...] {
+        // set에 추가하고자 하는 값이 있다면 추가하지 않고, 없다면 추가
+        str_set.update(with: s[i...j])
     }
-    
-    
 }
 
 print(str_set.count)
