46e3a92b3ccc4c7f98cc7c020b7624ae1404b5d5
Eric <real.dassy@gmail.com>
2024-05-02 14:40:58 +0900

    [1934]
    재귀함수로 최대공약수를 굉장히 간단하게 구하는 모습..

--- Algorithm/BaekJoon/[1934]/main.swift
+++ Algorithm/BaekJoon/[1934]/main.swift
@@ -5,8 +5,6 @@
 //  Created by 송지혁 on 5/2/24.
 //
 
-import Foundation
-
 let t = Int(readLine()!)!
 var result = ""
 
@@ -20,61 +18,18 @@ for _ in 0..<t {
 
 print(result)
 
-// 1. 각 수의 약수를 구한다.
-// 2. 공약수를 구한다.
-// 3. 최대 공약수를 구한다.
-// 4. 둘의 곱을 최대 공약수로 나눈다.
-
 
 // 최소 공배수 구하기
 func getLeastCommonMultiple(_ num1: Int, _ num2: Int) -> Int {
-    let measures1 = getMeasures(Double(num1))
-    let measures2 = getMeasures(Double(num2))
-    let commonFactor = getCommonFactor(measures1, measures2)
-    let greatestCommonFactor = getGreatestCommonFactor(commonFactor)
+    let greatestCommonFactor = gcd(num1, num2)
     
     
     return (num1 * num2) / greatestCommonFactor
 }
 
-
-// 약수 구하기
-func getMeasures(_ num: Double) -> [Int] {
-    var measures: [Int] = []
-    
-    for i in 1...Int(sqrt(num)) {
-        if Int(num) % i == 0 {
-            if i*i != Int(num) {
-                measures.append(Int(num) / i)
-            }
-            
-            measures.append(i)
-        }
-    }
-    
-    return measures
-}
-
-// 공약수 구하기
-private func getCommonFactor(_ measures1: [Int], _ measures2: [Int]) -> [Int] {
-    var commonFactor: [Int] = []
-    let measures1_dict = convertArrToDict(measures1)
-    
-    measures2.forEach {
-        if measures1_dict[$0] != nil {
-            commonFactor.append($0)
-        }
-    }
-    
-    return commonFactor
-}
-
-// arr -> dict
-private func convertArrToDict<T>(_ arr: [T]) -> Dictionary<T, Int> {
-    return Dictionary(uniqueKeysWithValues: arr.map { ($0, 0) })
-}
-
 // 최대 공약수 구하기
-private func getGreatestCommonFactor(_ commonFactor: [Int] = [0]) -> Int {
-    return commonFactor.isEmpty ? 1 : commonFactor.max()!
+func gcd(_ num1: Int, _ num2: Int) -> Int {
+    if num1 % num2 == 0 { return num2 }
+    
+    return gcd(num2, num1 % num2)
 }
