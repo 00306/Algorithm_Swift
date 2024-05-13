d520c687183d55e34dc0a82778ef31a2c190e992
Eric <real.dassy@gmail.com>
2024-05-13 13:10:09 +0900

    [11866] 시간 효율 개선
    queue에서 직접 더하고 빼는 시간을 index의 로직을 활용해서 줄였습니다.

--- Algorithm/BaekJoon/[11866]/main.swift
+++ Algorithm/BaekJoon/[11866]/main.swift
@@ -5,52 +5,17 @@
 //  Created by 송지혁 on 5/13/24.
 //
 
-class Queue<T> {
-    private var array: [T] = []
-    var index = 0
-    
-    func enqueue(_ element: T) {
-        array.append(element)
-    }
-    
-    func dequeue() -> T? {
-        defer {
-            index += 1
-        }
-        
-        return array[index]
-    }
-    
-    var count: Int {
-        array.count - index
-    }
-    
-    var isEmpty: Bool {
-        array[index...].isEmpty
-    }
-    
-    var peek: T? {
-        array[index]
-    }
-}
-
 let input = readLine()!.split(separator: " ").map { Int($0)! }
 let peopleCount = input[0]
 let removeNumber = input[1]
-var queue = Queue<Int>()
 
+var peoples = Array(1...input[0])
 var result: [String] = []
 
-for i in 1...peopleCount {
-    queue.enqueue(i)
-}
-
-while queue.count != 0 {
-    for _ in 0..<removeNumber-1 {
-        queue.enqueue(queue.dequeue()!)
-    }
-    
-    result.append(String(queue.dequeue()!))
+var index = 0
+while peoples.count != 0 {
+    index = (index + removeNumber - 1) % peoples.count
+    result.append("\(peoples.remove(at: index))")
 }
 
 print("<\(result.joined(separator: ", "))>")
