d56cd7455067d9fe99493670af8b0f75fd7acb62
Eric <real.dassy@gmail.com>
2024-05-15 17:04:40 +0900

    [1037]
    약수 뜻 모르나

--- Algorithm/BaekJoon/[1037]/main.swift
+++ Algorithm/BaekJoon/[1037]/main.swift
@@ -7,25 +7,5 @@
 
 let n = Int(readLine()!)!
 let input = readLine()!.split(separator: " ").map { Int($0)! }
-var lcmNumber = input[0]
 
-if n == 1 {
-  print(input[0] * input[0])
-} else {
-    for i in 0..<n-1 {
-        lcmNumber = lcm(lcmNumber, input[i+1])
-    }
-    
-    print(input.contains(lcmNumber) ? lcmNumber * input.min()! : lcmNumber)
-}
-
-func gcd(_ num1: Int, _ num2: Int) -> Int {
-    if num1 % num2 == 0 { return num2 }
-    
-    
-    return gcd(num2, num1 % num2)
-}
-
-func lcm(_ num1: Int, _ num2: Int) -> Int {
-    return num1 * num2 / gcd(num1, num2)
-}
+print(input.min()! * input.max()!)
