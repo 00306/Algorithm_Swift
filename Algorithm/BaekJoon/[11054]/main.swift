d823c3e4969984951bba9faa7a16df52900fac3d
Eric <real.dassy@gmail.com>
2024-06-13 11:32:36 +0900

    [11054]

--- Algorithm/BaekJoon/[11054]/main.swift
+++ Algorithm/BaekJoon/[11054]/main.swift
@@ -7,5 +7,31 @@
 
 let n = Int(readLine()!)!
 let sequence = readLine()!.split(separator: " ").map { Int($0)! }
+var dp = Array(repeating: Array(repeating: 1, count: n), count: 2)
+var bitonic = Array(repeating: 1, count: n)
 
 
+
+    for i in 0..<n {
+        for j in 0..<i {
+            if sequence[i] > sequence[j] {
+                dp[0][i] = max(dp[0][i], dp[0][j] + 1)
+            }
+        }
+        
+        for j in n-i..<n {
+            if sequence[n-i-1] > sequence[j] {
+                dp[1][n-i-1] = max(dp[1][n-i-1], dp[1][j] + 1)
+            }
+            
+        }
+        
+    }
+
+if n > 1 {
+    for i in 0..<n {
+        bitonic[i] = dp[0][i] + dp[1][i] - 1
+    }
+}
+
+print(bitonic.max()!)
