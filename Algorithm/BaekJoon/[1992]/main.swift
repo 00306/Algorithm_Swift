96e78a6098c88ee8cb51db6b3fe4b9e9aecf0f3f
Eric <real.dassy@gmail.com>
2024-08-25 17:15:20 +0900

    [1992]

--- Algorithm/BaekJoon/[1992]/main.swift
+++ Algorithm/BaekJoon/[1992]/main.swift
@@ -5,4 +5,31 @@
 //  Created by 송지혁 on 9/12/24.
 //
 
-import Foundation
+let n = Int(readLine()!)!
+var map: [[Int]] = []
+var result = ""
+
+for _ in 0..<n {
+    let line = readLine()!.map { Int(String($0))! }
+    map.append(line)
+}
+
+
+func explore(x: Int, y: Int, n: Int) -> String {
+    let number = map[y][x]
+    
+    for i in y..<y+n {
+        for j in x..<x+n {
+            if map[i][j] != number {
+                return "(" + explore(x: x, y: y, n: n/2) + explore(x: x+(n/2), y: y, n: n/2) + 
+                explore(x: x, y: y+(n/2), n: n/2) + explore(x: x+(n/2), y: y+(n/2), n: n/2) + ")"
+            }
+        }
+    }
+    
+    return String(number)
+}
+
+
+print(explore(x: 0, y: 0, n: n))
+
