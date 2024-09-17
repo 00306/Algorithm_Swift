70f5200b84d938f1bc099a6191cd5777d38a12d3
Eric <real.dassy@gmail.com>
2024-09-17 21:21:10 +0900

    [141] Linked List Cycle

---
+++ Algorithm/LeetCode/[141] Linked List Cycle/main.swift
@@ -0,0 +1,31 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 송지혁 on 10/21/24.
+//
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = ni
+    }
+}
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var slowNode = head
+        var fastNode = head
+        
+        while fastNode != nil && fastNode?.next != nil {
+            slowNode = slowNode?.next
+            fastNode = fastNode?.next?.next
+            if slowNode === fastNode { return true }
+            
+        }
+        
+        return false
+    }
+}
